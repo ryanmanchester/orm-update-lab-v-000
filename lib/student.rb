4d5d424aac56668ad504709d4dc388c9a1f4786d
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 20:53:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,4 +9,16 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+
+  end
+
 end
