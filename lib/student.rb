c4fef5e8a1cf23e5e2170dc5a485231b63b28806
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 21:36:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
     SELECT * FROM students
     WHERE name = ?
     SQL
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}
   end
 
   def save
