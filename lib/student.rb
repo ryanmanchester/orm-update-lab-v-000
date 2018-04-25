299a7065f2dd78d39834816866299c24a488e9b5
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 21:35:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,11 @@ class Student
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+    SELECT * FROM studnets
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name)
   end
 
   def save
